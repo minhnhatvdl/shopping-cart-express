a779361c5f49858e9ed2b9c146054694e5ecfd3c
minhnhatvdl <minhnhatvdl@gmail.com>
2019-03-30 22:12:31 +0100

    add a new page

--- routes/admin-pages.js
+++ routes/admin-pages.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('admin/pages', { title: 'Admin page' });
+router.get("/", (req, res, next) => {
+  res.render("admin/pages", { title: "Admin page" });
 });
 
+// add page
+router.get("/add-page", (req, res, next) => {
+  const title = "ddd";
+  const slug = "ddd";
+  const content = "ddd";
+  res.render("admin/addPage", { title, slug, content });
+});
+
+// export
 module.exports = router;
